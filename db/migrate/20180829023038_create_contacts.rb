75405fca452f75573ca97d6be8bc0c16dd4c7735
Matt Wilkinson <mytematt@gmail.com>
2018-08-28 20:34:15 -0600

    create database and add 1st contact

---
+++ db/migrate/20180829023038_create_contacts.rb
@@ -0,0 +1,16 @@
+class CreateContacts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.integer :age
+      t.string :phone
+      t.string :email
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip
+
+      t.timestamps
+    end
+  end
+end
